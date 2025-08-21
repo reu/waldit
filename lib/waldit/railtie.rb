6a3b55b9cdc70d791a3bb8a81e261c873c353c87
Rodrigo Navarro <rnavarro1@gmail.com>
2025-08-21 10:58:40 -0300

    Adding adapter alias

--- lib/waldit/railtie.rb
+++ lib/waldit/railtie.rb
@@ -5,11 +5,13 @@ require "rails/railtie"
 module Waldit
   class Railtie < Rails::Railtie
     config.before_configuration do
-      ActiveRecord::ConnectionAdapters.register(
-        "postgresqlwaldit",
-        "Waldit::PostgreSQLAdapter",
-        "waldit/postgresql_adapter",
-      )
+      ["waldit", "postgresqlwaldit"].each do |adapter_name|
+        ActiveRecord::ConnectionAdapters.register(
+          adapter_name,
+          "Waldit::PostgreSQLAdapter",
+          "waldit/postgresql_adapter",
+        )
+      end
     end
   end
 end
