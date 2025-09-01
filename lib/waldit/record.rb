a2183eb2d554726d54b4a554ad68c5fa418758a4
Rodrigo Navarro <rnavarro1@gmail.com>
2025-09-01 10:20:36 -0300

    Adds additional scopes to Waldit::Record

--- lib/waldit/record.rb
+++ lib/waldit/record.rb
@@ -2,21 +2,28 @@
 
 module Waldit
   module Record
+    extend ActiveSupport::Concern
+
+    included do
+      default_scope { order(:committed_at) }
+      scope :from_table, -> table { where(table_name: table) }
+      scope :from_model, -> model { from_table(model.table_name) }
+      scope :for, -> record { from_model(record.class).where(primary_key: record.id) }
+      scope :with_context, -> ctx { where("context @> ?", ctx.to_json) }
+    end
+
     def new
       return self[:new] if self[:new]
-
       (self[:diff] || {}).transform_values { |_old, new| new }
     end
 
     def old
       return self[:old] if self[:old]
-
       (self[:diff] || {}).transform_values { |old, _new| old }
     end
 
     def diff
       return self[:diff] if self[:diff]
-
       (old.keys | new.keys).reduce({}) do |diff, key|
         old[key] != new[key] ? diff.merge(key => [old[key], new[key]]) : diff
       end
